62bc597e8d9626d9cf405a4bd047a4a310e3d435
RobertStoelhorst <robbiestoelhorst@hotmail.com>
2021-01-31 10:17:55 +1100

    dump unused code

--- App.js
+++ App.js
@@ -100,15 +100,6 @@ const App = () => {
     transformData(groupBy(data, "date"))
   );
 
-  // useEffect(() => {
-  //   const total = gigs.reduce((total, gig) => {
-  //     return total + gig.amount;
-  //   }, 0);
-
-  //   setTotal(total);
-  // }, [gigs]);
-
-  // I have re-written the above ⬆ useEffect function in a more elegant way ⬇
   useEffect(() => {
     setTotal(gigs.reduce((total, gig) => total + Number(gig.amount), 0));
   }, [gigs]);
@@ -123,36 +114,17 @@ const App = () => {
       },
     ]);
 
+    setData([
+      ...data,
+      {
+        date: moment().format("LL"),
+        amount: Number(amount),
+      },
+    ]);
+
     setDescription("");
     setAmount("");
   };
-
-  // const data = {
-  //   labels: ["January", "February", "March", "April", "May", "June"],
-  //   datasets: [
-  //     {
-  //       data: [20, 45, 28, 80, 99, 43],
-  //     },
-  //   ],
-  // };
-
-  // const chartConfig = {
-  //   backgroundGradientFrom: "#1E2923",
-  //   backgroundGradientFromOpacity: 0,
-  //   backgroundGradientTo: "#08130D",
-  //   backgroundGradientToOpacity: 0.5,
-  //   color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-  //   strokeWidth: 2, // optional, default 3
-  //   barPercentage: 0.5,
-  //   useShadowColorFromDataset: false, // optional
-  // };
-  // const [input, setInput] = useState("");
-  // const [todos, setTodos] = useState([]);
-
-  // const addTodo = () => {
-  //   setTodos([input, ...todos]);
-  //   setInput("");
-  // };
 
   return (
     <SafeAreaProvider>
@@ -212,17 +184,6 @@ const App = () => {
           >
             total Income: ${total.toFixed(2)}
           </Text>
-          {/* for the todos to generate succesfully they also need to generate a key rather than using the index like I have below, using the index is frowned upon there is a unique key generator install library package called UUIDV4 */}
-          {/* <ScrollView>
-          {todos.map((todo, index) => (
-            <Todo key={index} title={todo} />
-          ))}
-        </ScrollView> */}
-          {/* <TextInput
-          style={styles.todoInput}
-          value={input}
-          onChangeText={(text) => setInput(text)}
-        /> */}
           <TextInput
             style={styles.input}
             value={description}
@@ -252,22 +213,6 @@ const App = () => {
               </ScrollView>
             </View>
           ))}
-          {/* <Button style={styles.todoButton} onPress={addTodo} title="Add TODO" /> */}
-
-          {/* <View>
-            <BarChart
-              // style={graphStyle}
-              data={data}
-              width={screenWidth}
-              height={220}
-              yAxisLabel="$"
-              chartConfig={chartConfig}
-              // verticalLabelRotation={30}
-            />
-          </View> */}
-
-          {/* </SafeAreaView> */}
-          {/* </SafeAreaProvider> */}
         </ScrollView>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -278,21 +223,16 @@ export default App;
 
 const styles = StyleSheet.create({
   titleText: {
-    // flex: 1,
     fontSize: 30,
     margin: "auto",
     paddingTop: 10,
     paddingBottom: 25,
     paddingLeft: 30,
     paddingRight: 30,
-    // backgroundColor: "pink",
-    // alignItems: "center",
-    // justifyContent: "center",
   },
   input: {
     marginLeft: 15,
     marginRight: 15,
-    // marginTop: 10,
     marginBottom: 10,
     height: 40,
     borderColor: "#3b38f5",
@@ -301,32 +241,8 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     backgroundColor: "whitesmoke",
   },
-
-  chartView: {
-    // borderColor: "red",
-    // borderWidth: 2,
-    // borderRadius: 16,
-  },
-
   jobButton: {
     margin: "auto",
     width: "40%",
-    // borderRadius: 16,
-    // borderColor: "#3b38f5",
-    // borderWidth: 1.5,
   },
-
-  // todoInput: {
-  //   margin: 20,
-  //   height: 40,
-  //   borderColor: "red",
-  //   padding: 20,
-  //   borderWidth: 2,
-  //   backgroundColor: "whitesmoke",
-  // },
-
-  // todoButton: {
-  //   margin: 20,
-  //   height: 40,
-  // },
 });
